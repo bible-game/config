7fa309c622586c0823ca4e22d82f8a7e6f4a34d1
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2025-10-11 14:37:01 +0100

    error

--- src/main/kotlin/game/bible/config/model/integration/BollsLifeApiConfig.kt
+++ src/main/kotlin/game/bible/config/model/integration/BollsLifeApiConfig.kt
@@ -2,35 +2,38 @@ package game.bible.config.model.integration
 
 import game.bible.config.bean.Initialisable
 import game.bible.config.bean.Reloadable
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.Configuration
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.Serial
 
 /**
  * Configuration for Bolls.Life Bible API
  * @since 11th October 2025
  */
-
-@Reloadable
-(
-  prefix = "bolls.api",
-  path = "\${application.config.dir}"
+@Reloadable(
+    prefix = "bolls.api",
+    path = "\${application.config.dir}"
 )
 class BollsLifeApiConfig : Initialisable {
 
-  init {
-    println("Created [${this.javaClass.name}]")
-  }
+    init {
+        log.info("Created [{}]", this.javaClass.name)
+    }
 
-  private var baseUrl: String = "https://bolls.life"
+    private var baseUrl: String = "https://bolls.life"
 
-  fun getBaseUrl(): String = baseUrl
-  fun setBaseUrl(url: String) {
-    baseUrl = url
-  }
+    fun getBaseUrl(): String = baseUrl
+    fun setBaseUrl(url: String) {
+        baseUrl = url
+    }
 
-  // API Endpoints
-  fun getBooksUrl(version: String): String = "$baseUrl/get-books/$version/"
-  fun getTextUrl(version: String, bookId: String, chapter: Int): String =
-    "$baseUrl/get-text/$version/$bookId/$chapter/"
+    // API Endpoints
+    fun getBooksUrl(version: String): String = "$baseUrl/get-books/$version/"
+    fun getTextUrl(version: String, bookId: String, chapter: Int): String =
+        "$baseUrl/get-text/$version/$bookId/$chapter/"
 
+    companion object {
+        @Serial private val serialVersionUID = 1L
+        private val log: Logger = LoggerFactory.getLogger(BollsLifeApiConfig::class.java)
+    }
 }
